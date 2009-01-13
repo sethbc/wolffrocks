f444051112425d6c70a1a012f9381f611fd5b332
Seth Chandler <sethbc@sethbc.org>
2009-01-13 03:14:20 -0500

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+
+default_run_options[:pty] = true
+
+# be sure to change these
+set :user, 'sethbc'
+set :domain, 'wolffrocks.com'
+set :application, 'wolffrocks'
+
+# the rest should be good
+set :repository,  "git@github.com:#{user}/#{application}.git" 
+set :deploy_to, "/home/#{user}/#{domain}" 
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
+set :scm_verbose, true
+set :use_sudo, false
+ssh_options[:forward_agent] = true
+
+server domain, :app, :web
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+end
